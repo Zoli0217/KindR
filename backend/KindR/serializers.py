6f713adaf868e89ba53592b883e11978e4f1590a
Zoli0217 <zoltanbodi61@gmail.com>
2026-03-03 12:54:32 +0100

    serializer

---
+++ backend/KindR/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "first_name", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            password=validated_data["password"],
+        )
+        return user
